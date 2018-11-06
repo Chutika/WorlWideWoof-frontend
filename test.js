f696fb256edb3ee4719c65b543b981d5574baf7f
Ooee <chutika.wera@hotmail.com>
2018-11-06 16:20:46 +0700

    adjust navbar and update intro-info-section(aboutShop)

---
+++ test.js
@@ -0,0 +1,19 @@
+function clickLogo(){
+    location.href="html/landingPage.html"
+}
+
+function clickShopMenu(){
+    location.href="html/forSale.html"
+    document.getElementById("shopMenu").style.backgroundColor = "#F1D102";
+    document.getElementById("foundMenu").style.backgroundColor = "#FFE32E";
+    document.getElementById("shopMenu").style.fontWeight="500";
+    document.getElementById("foundMenu").style.fontWeight="normal";
+
+}
+function clickFoundationMenu(){
+    location.href="html/forAdopt.html"
+    document.getElementById("shopMenu").style.backgroundColor = "#FFE32E";
+    document.getElementById("foundMenu").style.backgroundColor = "#F1D102";
+    document.getElementById("shopMenu").style.fontWeight="normal";
+    document.getElementById("foundMenu").style.fontWeight="500";
+}
