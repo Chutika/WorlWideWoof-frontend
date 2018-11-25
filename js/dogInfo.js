c00561a36019d31affe470fcf841f5d704fe5222
MeowKawaii <pingpong_jane@hotmail.com>
2018-11-26 00:07:55 +0700

    edit dog info and dog info for shop

--- js/dogInfo.js
+++ js/dogInfo.js
@@ -1,28 +1,30 @@
-function clickSameDog(){
-  location.href="dogInfo.html"
-}
-
-function goToPage(){
-  location.href="shopInfo.html"
-}
-
-
 var slideIndex = 1;
 showSlides(slideIndex);
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides(n);
 }
+function clickSameDog(){
+  location.href="dogInfo.html";
+}
+
+function goToPage(){
+  location.href="shopInfo.html";
+}
+
+function gotoInfoShop(){
+  location.href="shopInfo.html";
+}
 
 function showSlides(n) {
     if (n==1){
-        document.getElementById("Labrador").src="https://dynaimage.cdn.cnn.com/cnn/w_768,h_1024,c_scale/https%3A%2F%2Fdynaimage.cdn.cnn.com%2Fcnn%2Fx_1085%2Cy_0%2Cw_2578%2Ch_3437%2Cc_crop%2Fhttps%253A%252F%252Fstamp.static.cnn.io%252F5b7ac48b4db3d70020c01c13%252Fshutterstock_757946224.jpg";
+        document.getElementById("mainPicture").src="https://dynaimage.cdn.cnn.com/cnn/w_768,h_1024,c_scale/https%3A%2F%2Fdynaimage.cdn.cnn.com%2Fcnn%2Fx_1085%2Cy_0%2Cw_2578%2Ch_3437%2Cc_crop%2Fhttps%253A%252F%252Fstamp.static.cnn.io%252F5b7ac48b4db3d70020c01c13%252Fshutterstock_757946224.jpg";
     }
     else if(n==2){
-        document.getElementById("Labrador").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk8n0D7Pb8Ns73Ztu_utsish7QxmgTy99PVuGNUzqQQfZJsSNn"
+        document.getElementById("mainPicture").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk8n0D7Pb8Ns73Ztu_utsish7QxmgTy99PVuGNUzqQQfZJsSNn";
     }
     else if(n==3){
-        document.getElementById("Labrador").src="https://dynaimage.cdn.cnn.com/cnn/w_768,h_1024,c_scale/https%3A%2F%2Fdynaimage.cdn.cnn.com%2Fcnn%2Fx_1229%2Cy_0%2Cw_2712%2Ch_3616%2Cc_crop%2Fhttps%253A%252F%252Fstamp.static.cnn.io%252F5b7ac48b4db3d70020c01c13%252Fshutterstock_1081879181.jpg"
+        document.getElementById("mainPicture").src="https://dynaimage.cdn.cnn.com/cnn/w_768,h_1024,c_scale/https%3A%2F%2Fdynaimage.cdn.cnn.com%2Fcnn%2Fx_1229%2Cy_0%2Cw_2712%2Ch_3616%2Cc_crop%2Fhttps%253A%252F%252Fstamp.static.cnn.io%252F5b7ac48b4db3d70020c01c13%252Fshutterstock_1081879181.jpg";
     }
   var i;
 //   var slides = document.getElementsByClassName("mySlides");
@@ -38,3 +40,5 @@ function showSlides(n) {
 //   slides[slideIndex-1].style.display = "block";
   dots[n-1].className += " active";
 }
+
+
