48cdec11ce8822d315ffd4adf3f5e97da4bc762f
MeowKawaii <pingpong_jane@hotmail.com>
2018-11-26 18:26:23 +0700

    test picture

--- js/dogInfo.js
+++ js/dogInfo.js
@@ -19,11 +19,11 @@ function gotoInfoShop() {
 
 function showSlides(n) {
   if (n == 1) {
-    document.getElementById("mainPicture").src = "https://dynaimage.cdn.cnn.com/cnn/w_768,h_1024,c_scale/https%3A%2F%2Fdynaimage.cdn.cnn.com%2Fcnn%2Fx_1085%2Cy_0%2Cw_2578%2Ch_3437%2Cc_crop%2Fhttps%253A%252F%252Fstamp.static.cnn.io%252F5b7ac48b4db3d70020c01c13%252Fshutterstock_757946224.jpg";
+    document.getElementById("mainPicture").src =document.getElementById("fontSide").src;
   } else if (n == 2) {
-    document.getElementById("mainPicture").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk8n0D7Pb8Ns73Ztu_utsish7QxmgTy99PVuGNUzqQQfZJsSNn";
+    document.getElementById("mainPicture").src =document.getElementById("picTwo").src;
   } else if (n == 3) {
-    document.getElementById("mainPicture").src = "https://dynaimage.cdn.cnn.com/cnn/w_768,h_1024,c_scale/https%3A%2F%2Fdynaimage.cdn.cnn.com%2Fcnn%2Fx_1229%2Cy_0%2Cw_2712%2Ch_3616%2Cc_crop%2Fhttps%253A%252F%252Fstamp.static.cnn.io%252F5b7ac48b4db3d70020c01c13%252Fshutterstock_1081879181.jpg";
+    document.getElementById("mainPicture").src =document.getElementById("picThree").src;
   }
   var i;
   //   var slides = document.getElementsByClassName("mySlides");
