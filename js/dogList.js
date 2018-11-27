35a15445bcca50727743e2bc1e6f5be4d45e0ad9
MeowKawaii <pingpong_jane@hotmail.com>
2018-11-27 23:08:56 +0700

    Revert "UI dog list"
    This reverts commit 4683b894047af49f8c283e1ad4b968b4d52e4541.

--- js/dogList.js
+++ js/dogList.js
@@ -3,9 +3,5 @@ $(function(){
 });
 
 function goToDogInfo_forShop(){
-    location.href="dogInfo_forShop.html";
-  }
-
-  function deleteDog(){
-    location.href="dogList.html";
+    location.href="dogInfo_forShop.html"
   }
