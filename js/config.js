3d0ef32143dabfd13f7e9f11abad82b9d607c9d4
ChiChaChai <chatchaishaetan@gmail.com>
2018-11-29 05:25:52 +0700

    Fix some display text

--- js/config.js
+++ js/config.js
@@ -1,3 +1,6 @@
 var config = {
-    host: "https://us-central1-worldwidewoof-bcdfa.cloudfunctions.net/app"
+    // host: "http:/localhost:5000",
+    host: "http://localhost:5000/worldwidewoof-bcdfa/us-central1/app",
+    
+
 }
