c09ccb03e4df722c29e996c1d54ec36b531b90a2
MeowKawaii <pingpong_jane@hotmail.com>
2018-11-29 09:26:28 +0700

    Auto stash before merge of "master" and "origin/master"

--- js/config.js
+++ js/config.js
@@ -1,6 +1,11 @@
 var config = {
+<<<<<<< Updated upstream
     host: "http://localhost:8080",
     // host: "http://localhost:5000/worldwidewoof-bcdfa/us-central1/app",
+=======
+    // host: "http:/localhost:5000",
+    host: "http://localhost:8080",
+>>>>>>> Stashed changes
     
 
 }
