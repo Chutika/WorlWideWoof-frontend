ed515b08265333d8e77997b22921cc136ad3d954
Ooee <chutika.wera@hotmail.com>
2018-11-29 08:05:26 +0700

    merge to current

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var config = {
-    // host: "http:/localhost:5000",
-    host: "http://localhost:5000/worldwidewoof-bcdfa/us-central1/app",
+    host: "http://localhost:8080",
+    // host: "http://localhost:5000/worldwidewoof-bcdfa/us-central1/app",
     
 
 }
