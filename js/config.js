abf8885a85511e9c1e0d35cac2a5e7c429a6ae09
ChiChaChai <chatchaishaetan@gmail.com>
2018-12-09 13:25:02 +0700

    Fix wrong field

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var config = {
     // host: "http:/localhost:5000",
     // host: "http://localhost:5000/worldwidewoof-bcdfa/us-central1/app",
-    host:"https://us-central1-worldwidewoof-bcdfa.cloudfunctions.net/app"
-    // host: "http://localhost:8080"
+    // host:"https://us-central1-worldwidewoof-bcdfa.cloudfunctions.net/app"
+    host: "http://localhost:8080"
 }
