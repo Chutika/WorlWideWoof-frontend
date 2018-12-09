6a3235c54a4795396cc90a908365b4a4979c18f6
ChiChaChai <chatchaishaetan@gmail.com>
2018-12-09 12:20:15 +0700

    Fix some css not render

--- js/config.js
+++ js/config.js
@@ -1,11 +1,6 @@
 var config = {
-<<<<<<< Updated upstream
-    host: "http://localhost:8080",
-    // host: "http://localhost:5000/worldwidewoof-bcdfa/us-central1/app",
-=======
     // host: "http:/localhost:5000",
-    host: "http://localhost:8080",
->>>>>>> Stashed changes
-    
-
+    // host: "http://localhost:5000/worldwidewoof-bcdfa/us-central1/app",
+    host:"https://us-central1-worldwidewoof-bcdfa.cloudfunctions.net/app"
+    // host: "http://localhost:8080"
 }
